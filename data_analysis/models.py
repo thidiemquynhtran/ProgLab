da45498340580f7dd51b4383f60d6a06ec2b0931
thidiemquynhtran <diemquynh.ttdq02@gmail.com>
2024-05-15 14:47:18 +0200

    -Create a superuser (admin) to check the connection to database -Successfully how products list in admin site

--- data_analysis/models.py
+++ data_analysis/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Customers(models.Model):
+class Customer(models.Model):
     customerid = models.CharField(db_column='customerID', primary_key=True, max_length=7)  # Field name made lowercase.
     latitude = models.CharField(max_length=18, blank=True, null=True)
     longitude = models.CharField(max_length=19, blank=True, null=True)
@@ -11,7 +11,7 @@ class Customers(models.Model):
         managed = True
         db_table = 'customers'
 
-class Products(models.Model):
+class Product(models.Model):
     sku = models.CharField(db_column='SKU', primary_key=True, max_length=5)  # Field name made lowercase.
     name = models.CharField(db_column='Name', max_length=21, blank=True, null=True)  # Field name made lowercase.
     price = models.DecimalField(db_column='Price', max_digits=4, decimal_places=2, blank=True, null=True)  # Field name made lowercase.
@@ -23,8 +23,11 @@ class Products(models.Model):
     class Meta:
         managed = True
         db_table = 'products'
+    def __str__(self):
+        return self.name
 
-class Stores(models.Model):
+
+class Store(models.Model):
     storeid = models.CharField(db_column='storeID', primary_key=True, max_length=7)  # Field name made lowercase.
     zipcode = models.IntegerField(db_column='zipcode', blank=True, null=True)
     state_abbr = models.CharField(db_column='state_abbr',max_length=2, blank=True, null=True)
@@ -38,10 +41,10 @@ class Stores(models.Model):
         managed = True
         db_table = 'stores'
 
-class Orders(models.Model):
+class Order(models.Model):
     orderid = models.IntegerField(db_column='orderID', primary_key=True)  # Field name made lowercase.
-    customerid = models.ForeignKey(Customers, on_delete=models.SET_NULL, db_column='customerID', blank=True, null=True)  # Field name made lowercase.
-    storeid = models.ForeignKey(Stores, on_delete=models.SET_NULL, db_column='storeID', blank=True, null=True)  # Field name made lowercase.
+    customerid = models.ForeignKey(Customer, on_delete=models.SET_NULL, db_column='customerID', blank=True, null=True)  # Field name made lowercase.
+    storeid = models.ForeignKey(Store, on_delete=models.SET_NULL, db_column='storeID', blank=True, null=True)  # Field name made lowercase.
     orderdate = models.CharField(db_column='orderDate', max_length=20, blank=True, null=True)  # Field name made lowercase.
     nitems = models.IntegerField(db_column='nItems', blank=True, null=True)  # Field name made lowercase.
     total = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
@@ -50,9 +53,9 @@ class Orders(models.Model):
         managed = True
         db_table = 'orders'
 
-class OrderItems(models.Model):
-    sku = models.ForeignKey(Products, on_delete=models.SET_NULL, db_column='SKU', blank=True, null=True)  # Field name made lowercase.
-    orderid = models.ForeignKey(Orders, on_delete=models.SET_NULL, db_column='orderID', blank=True, null=True)  # Field name made lowercase.
+class OrderItem(models.Model):
+    sku = models.ForeignKey(Product, on_delete=models.SET_NULL, db_column='SKU', blank=True, null=True)  # Field name made lowercase.
+    orderid = models.ForeignKey(Order, on_delete=models.SET_NULL, db_column='orderID', blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
         managed = True
