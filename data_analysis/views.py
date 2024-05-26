b1b1d365463c026daa1cb5015ce2473ac98f91bc
Ayazrr31 <aya.zirari@stud.fra-uas.de>
2024-05-26 17:35:54 +0200

    updated apis+urls

--- data_analysis/views.py
+++ data_analysis/views.py
@@ -4,33 +4,49 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .utils import (
     calculate_total_customers,
-    calculate_average_sales_revenue,
-    get_total_sales_by_year_with_filters,
+    calculate_average_order_value,
+    calculate_repeat_purchase_rate,
+    calculate_total_revenue,
+    get_total_sales_by_month_with_filters,
     get_total_sales_by_size_with_filters,
     get_total_sales_by_product_with_filters,
     get_pizza_category_distribution,
+    get_monthly_sales_by_category,
     get_total_sales_by_state
 )
 
 def index_view(request):
     return render(request, 'index.html')
 
+#Returns total number of customers
 @api_view(['GET'])
 def total_customers_view(request):
     total_customers = calculate_total_customers()
-
     return Response({'total_customers': total_customers})
 
+#Returns average order value
 @api_view(['GET'])
-def average_sales_revenue_view(request):
-    average_sales_revenue = calculate_average_sales_revenue()
-    return Response({'average_sales_revenue': average_sales_revenue})
+def average_order_value_view(request):
+    average_order_value = calculate_average_order_value()
+    return Response({'average_order_value': average_order_value})
 
+#Returns repeat purchase rate
 @api_view(['GET'])
-def total_sales_by_year_view(request):
+def repeat_purchase_rate_view(request):
+    repeat_purchase_rate = calculate_repeat_purchase_rate()
+    return Response({'repeat_purchase_rate': repeat_purchase_rate})
+
+#Returns total revenue
+@api_view(['GET'])
+def total_revenue_view(request):
+    total_revenue = calculate_total_revenue()
+    return Response({'total_revenue': total_revenue})
+
+#Returns total sales by month, optional year filter
+@api_view(['GET'])
+def total_sales_by_month_view(request):
     year = request.query_params.get('year')
-    product = request.query_params.get('product')
-    data = get_total_sales_by_year_with_filters(year, product)
+    data = get_total_sales_by_month_with_filters(year)
     return Response(data)
 
 @api_view(['GET'])
@@ -49,9 +65,13 @@ def total_sales_by_product_view(request):
 
 @api_view(['GET'])
 def pizza_category_distribution_view(request):
-    state = request.query_params.get('state')
-    year = request.query_params.get('year')
-    data = get_pizza_category_distribution(state, year)
+    data = get_pizza_category_distribution()
+    return Response(data)
+
+@api_view(['GET'])
+def monthly_sales_by_category_view(request):
+    category = request.query_params.get('category')
+    data = get_monthly_sales_by_category(category)
     return Response(data)
 
 @api_view(['GET'])
--- datatool/urls.py
+++ datatool/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path, include
+from data_analysis import views as data_views
 
 urlpatterns = [
-    path('', include('data_analysis.urls')), 
+    path('admin/', admin.site.urls),
+    path('', data_views.index_view, name='index'),
     path('api/', include('data_analysis.urls')),
 ]
 
