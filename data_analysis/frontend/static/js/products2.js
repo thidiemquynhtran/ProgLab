96d6fec1a61d7c49acf2cbba0b9e93b8b85352f8
thidiemquynhtran <diemquynh.ttdq02@gmail.com>
2024-07-15 16:20:49 +0200

    Created bar-line chart for popularity of product size

--- data_analysis/frontend/static/js/products2.js
+++ data_analysis/frontend/static/js/products2.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
-  const ctx = document.getElementById("ingredientUsageChart").getContext("2d");
+  // Ingredient Usage Chart
+  const ingredientUsageChart = echarts.init(
+    document.getElementById("ingredientUsageChart")
+  );
 
   $.ajax({
     url: "http://127.0.0.1:8000/ingredient-usage/",
@@ -10,56 +13,143 @@ $(document).ready(function () {
       const usageCounts = data.map((item) => item.usage_count);
       const usagePercentages = data.map((item) => item.usage_percentage);
 
-      new Chart(ctx, {
-        type: "bar",
-        data: {
-          labels: ingredients,
-          datasets: [
-            {
-              label: "Usage Count",
-              data: usageCounts,
-              backgroundColor: "skyblue",
-              borderColor: "blue",
-              borderWidth: 1,
-            },
-          ],
+      const ingredientUsageOption = {
+        title: {
+          text: "Ingredient Usage Count",
         },
-        options: {
-          scales: {
-            x: {
-              beginAtZero: true,
-              ticks: {
-                autoSkip: false,
-              },
-            },
-            y: {
-              beginAtZero: true,
-              position: "left",
-              title: {
-                display: true,
-                text: "Usage Count",
-              },
-            },
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            type: "shadow",
           },
-          plugins: {
-            title: {
-              display: true,
-              text: "Ingredient Usage Count",
-            },
-            tooltip: {
-              callbacks: {
-                afterLabel: function (tooltipItem) {
-                  const index = tooltipItem.dataIndex;
-                  return "Usage Percentage: " + usagePercentages[index] + "%";
-                },
-              },
-            },
+          formatter: function (params) {
+            let tooltipText = params[0].name + "<br>";
+            params.forEach((item) => {
+              tooltipText +=
+                item.marker + item.seriesName + ": " + item.value + "<br>";
+              if (item.seriesName === "Usage Count") {
+                const index = ingredients.indexOf(item.name);
+                tooltipText +=
+                  "Usage Percentage: " + usagePercentages[index] + "%<br>";
+              }
+            });
+            return tooltipText;
           },
         },
-      });
+        xAxis: {
+          type: "category",
+          data: ingredients,
+        },
+        yAxis: {
+          type: "value",
+          name: "Usage Count",
+        },
+        series: [
+          {
+            name: "Usage Count",
+            type: "bar",
+            data: usageCounts,
+            itemStyle: {
+              color: "skyblue",
+            },
+          },
+        ],
+      };
+
+      ingredientUsageChart.setOption(ingredientUsageOption);
     },
     error: function (error) {
       console.error("Error fetching data:", error);
+    },
+  });
+
+  // Product Size Popularity Chart
+  const productSizeChart = echarts.init(
+    document.getElementById("productSizeChart")
+  );
+
+  $.ajax({
+    url: "http://127.0.0.1:8000/product_size_popularity/",
+    method: "GET",
+    dataType: "json",
+    success: function (response) {
+      const sizes = [];
+      const numberOfItems = [];
+      const totalRevenue = [];
+
+      response.forEach((item) => {
+        sizes.push(item.product_size);
+        numberOfItems.push(item.total_sales);
+        totalRevenue.push(parseFloat(item.total_revenue));
+      });
+
+      const productSizeOption = {
+        title: {
+          text: "Product Size Popularity",
+        },
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            type: "cross",
+          },
+          formatter: function (params) {
+            let tooltipText = params[0].name + "<br>";
+            params.forEach((item) => {
+              tooltipText +=
+                item.marker + item.seriesName + ": " + item.value + "<br>";
+            });
+            return tooltipText;
+          },
+        },
+        grid: {
+          left: "18%",
+          right: "22%",
+          bottom: "15%",
+        },
+        xAxis: {
+          type: "category",
+          data: sizes,
+        },
+        yAxis: [
+          {
+            type: "value",
+            name: "Number of Items Sold",
+            position: "left",
+          },
+          {
+            type: "value",
+            name: "Total Revenue ($)",
+            position: "right",
+            axisLabel: {
+              formatter: "${value}",
+            },
+          },
+        ],
+        series: [
+          {
+            name: "Number of Items Sold",
+            type: "bar",
+            data: numberOfItems,
+            itemStyle: {
+              color: "rgba(54, 162, 235, 0.6)",
+            },
+          },
+          {
+            name: "Total Revenue ($)",
+            type: "line",
+            yAxisIndex: 1,
+            data: totalRevenue,
+            itemStyle: {
+              color: "rgba(255, 99, 132, 1)",
+            },
+          },
+        ],
+      };
+
+      productSizeChart.setOption(productSizeOption);
+    },
+    error: function (error) {
+      console.log("Error fetching data:", error);
     },
   });
 });
